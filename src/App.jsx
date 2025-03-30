84927153f384efe8b3d7ddd9962e15e5944f3412
Pranayeee <pranayeepawar@gmail.com>
2025-03-30 08:46:18 +0530

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
-import './styles/main.css';
+import './Styles/main.css';
 
 
 const App = () => {
